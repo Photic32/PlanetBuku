80eb7910f668a056d78a530d62ae07a33770ddee
almayawn <alma.laras@ui.ac.id>
2023-10-27 18:38:34 +0700

    made detail view slightly bearable (working)

--- view_book/models.py
+++ view_book/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from book.models import Book
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.models import User
 
 class Review(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
-    # user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     review = models.TextField()
     rate = models.IntegerField(
         validators=[MaxValueValidator(10), MinValueValidator(0)]
