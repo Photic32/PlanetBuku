ce262e4a9a5a90e8f90ee06bd785313287c1ffa8
claraa26 <clarasistaa26@gmail.com>
2023-10-29 02:39:28 +0700

    kurang kesan

--- borrowed_book_list/views.py
+++ borrowed_book_list/views.py
@@ -22,17 +22,32 @@ def borrowed_book_list(request):
 @login_required
 def return_book(request, book_id):
     # Retrieve the book object by its ID.
-    book = get_object_or_404(Book, pk=book_id)
+    peminjaman = get_object_or_404(Peminjaman, pk=book_id)
 
     # Check if the book is currently marked as "dipinjam."
-    if book.status == 'dipinjam':
+    if peminjaman.status == 'dipinjam':
         # Update the status to "dikembalikan."
-        book.status = 'dikembalikan'
-        book.save()
-        return HttpResponse("Book marked as returned successfully.")
+        peminjaman.status = 'dikembalikan'
+        peminjaman.save()
+        # return HttpResponse("Book marked as returned successfully.")
+        # return HttpResponseNotFound()
     return HttpResponseRedirect(reverse('borrowed_book_list:borrowed_book_list'))
 
+
 @csrf_exempt
+# def add_kesan_ajax(request,id):
+#     if request.method == 'POST':
+#         # book = request.book
+#         peminjaman = get_object_or_404(Peminjaman, pk=id)
+#         book_id = request.POST.get("book_id")
+#         kesan = request.POST.get("kesan")
+#         # book = get_object_or_404(Book, pk=book_id)
+#         new_kesan = KesanPeminjam(peminjaman=peminjaman, kesan=kesan)
+#         new_kesan.save()
+
+#         return HttpResponse(b"CREATED", status=201)
+
+#     return HttpResponseNotFound()
 def add_kesan_ajax(request):
     if request.method == 'POST':
         # book = request.book
