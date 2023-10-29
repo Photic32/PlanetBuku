c1aee4b5788eb0e72be0ed8f2fbd944be1b6d4a2
claraa26 <clarasistaa26@gmail.com>
2023-10-29 19:33:22 +0700

    coba push lagii

--- borrowed_book_list/urls.py
+++ borrowed_book_list/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from borrowed_book_list.views import borrowed_book_list, add_kesan_ajax, get_kesan_json, return_book
+from borrowed_book_list.views import borrowed_book_list, add_kesan_ajax, get_kesan_json, return_book, borrowed_book_json
 app_name = 'borrowed_book_list'
 
 urlpatterns = [
     path('', borrowed_book_list, name='borrowed_book_list'),
     path('return-book/<int:book_id>', return_book, name='return_book'),
-    path('get-kesan/', get_kesan_json, name='get_kesan_json'),
-    path('create-kesan-ajax/', add_kesan_ajax, name='add_kesan_ajax')
+    path('get-kesan/<int:id>', get_kesan_json, name='get_kesan_json'),
+    path('create-kesan-ajax/', add_kesan_ajax, name='add_kesan_ajax'),
+    path('borrowed-book-json/', borrowed_book_json, name='borrowed_book_json')
 ]
