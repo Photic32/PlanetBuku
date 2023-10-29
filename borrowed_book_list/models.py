57d6617422f0e360a62686c784d8ef7ae1ea6f4c
Muhammad Fachrudin Firdaus <firdausfarul@gmail.com>
2023-10-29 23:51:58 +0700

    ngurusin migrate

--- borrowed_book_list/models.py
+++ borrowed_book_list/models.py
@@ -11,14 +11,10 @@ def in_seven_days():
 class Peminjaman(models.Model): # App 5
     pengguna = models.ForeignKey(User, on_delete=models.CASCADE)
     tanggal_peminjaman = models.DateField(auto_now_add=True)
-    tanggal_pengembalian = models.DateField(in_seven_days)
+    tanggal_pengembalian = models.DateField(null=True, blank=True)
     buku = models.ForeignKey(Book, on_delete=models.CASCADE)
     status = models.CharField(max_length=255, choices=[('dipinjam', 'Dipinjam'), ('dikembalikan', 'Dikembalikan')])
 
-    def save(self, *args, **kwargs):
-        if self.tanggal_pengembalian is None:
-            self.tanggal_pengembalian = self.tanggal_peminjaman.date() + datetime.timedelta(days=7)
-        super(Peminjaman, self).save(*args, **kwargs)
 class KesanPeminjam(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     kesan = models.TextField()
