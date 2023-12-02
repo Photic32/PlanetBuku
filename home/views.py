bd56580829eb2341540a28de3cd13e72486546aa
Photic23 <resellcourtesyx@gmail.com>
2023-12-03 06:11:05 +0700

    minor fix

--- home/views.py
+++ home/views.py
@@ -46,87 +46,24 @@ def get_keranjang_json(request):
     keranjangs = Keranjang.objects.filter(user=request.user)
     return HttpResponse(serializers.serialize('json', keranjangs))
 
-@login_required(login_url='/register')
-def show_home(request):
-    user = request.user
-    if user.is_staff == True:
-        context = {
-            'last_login': request.COOKIES['last_login'],
-            'username' : user.username,
-        }
-
-        return render(request, "homeAdmin.html", context)
-    else:
-        context = {
-            'last_login': request.COOKIES['last_login'],
-            'username' : user.username,
-        }
-
-        return render(request, "homeUser.html", context)
+# @login_required(login_url='/register')
+# def show_home(request):
+#     user = request.user
+#     if user.is_staff == True:
+#         context = {
+#             'last_login': request.COOKIES['last_login'],
+#             'username' : user.username,
+#         }
 
 
-def register(request):
-    form = UserCreationForm()
+#         return render(request, "homeAdmin.html", context)
+#     else:
+#         context = {
+#             'last_login': request.COOKIES['last_login'],
+#             'username' : user.username,
+#         }
 
-    if request.method == "POST":
-        if request.POST.get("submitButton") =="Register":
-            form = UserCreationForm(request.POST)
-            if form.is_valid():
-                form.save()
-                messages.success(request, 'Your account has been successfully created!')
-                return redirect('home:register')
-            else:
-                messages.info(request, 'Sorry, incorrect username or password. Please try again.')
-        elif request.POST.get("submitButton") =="Login":
-            username = request.POST.get('username')
-            password = request.POST.get('password')
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                response = HttpResponseRedirect(reverse("home:show_home")) 
-                response.set_cookie('last_login', str(datetime.datetime.now()))
-                return response
-            else:
-                messages.info(request, 'Sorry, incorrect username or password. Please try again.')
-    context = {'form':form}
-    return render(request, 'login.html', context)
-
-def logout_user(request):
-    logout(request)
-    response = HttpResponseRedirect(reverse('home:register'))
-    response.delete_cookie('last_login')
-    return response
-# Create your views here.
-
-def get_book_json(request):
-    books = Book.objects.filter(stock__gt=0)
-    return HttpResponse(serializers.serialize('json', books))
-
-def get_peminjaman_json(request):
-    peminjamans = Peminjaman.objects.filter(pengguna=request.user)
-    return HttpResponse(serializers.serialize('json', peminjamans))
-
-def get_keranjang_json(request):
-    keranjangs = Keranjang.objects.filter(user=request.user)
-    return HttpResponse(serializers.serialize('json', keranjangs))
-
-@login_required(login_url='/register')
-def show_home(request):
-    user = request.user
-    if user.is_staff == True:
-        context = {
-            'last_login': request.COOKIES['last_login'],
-            'username' : user.username,
-        }
-
-        return render(request, "homeAdmin.html", context)
-    else:
-        context = {
-            'last_login': request.COOKIES['last_login'],
-            'username' : user.username,
-        }
-
-        return render(request, "homeUser.html", context)
+#         return render(request, "homeUser.html", context)
 
 
 def register(request):
@@ -161,6 +98,7 @@ def logout_user(request):
     response.delete_cookie('last_login')
     return response
 # Create your views here.
+
 
 def show_profile(request):
     if request.user.is_authenticated:
@@ -231,7 +169,7 @@ def show_keranjang(request):
     else:                                     
         return render(request,"homeGuest.html")
 
-
+@login_required(login_url='/register')
 def show_home(request):
     if request.user.is_authenticated:
         user = request.user
@@ -247,7 +185,6 @@ def show_home(request):
                 'last_login': request.COOKIES['last_login'],
                 'username' : user.username,
             }
-
             return render(request, "homeUser.html", context)
     else:                                       #kalo guest
         return render(request,"homeGuest.html")
