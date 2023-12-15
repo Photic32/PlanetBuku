6391a21b347585077231cc7b7b98f17c8ca35ae3
Marsya Rahmadani <marsyarahmadani12@gmail.com>
2023-12-15 19:53:24 +0700

    edit info flutter

--- edit_info/views.py
+++ edit_info/views.py
@@ -1,3 +1,4 @@
+import json
 from django.http import HttpResponseNotFound, HttpResponseRedirect, JsonResponse
 from django.http import HttpResponse
 from django.urls import reverse
@@ -7,6 +8,7 @@ from book.models import Book
 from edit_info.forms import BookForm
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.admin.views.decorators import staff_member_required
+from django.views.decorators.http import require_GET, require_POST
 
 
 def get_books_json(request):
@@ -132,3 +134,78 @@ def delete_book_ajax(request, id):
             return HttpResponse("Buku tidak ada", status=404)
 
     return HttpResponseNotFound()
+
+@csrf_exempt
+def create_book_flutter(request):
+    if request.method == 'POST':
+        
+        data = json.loads(request.body)
+
+        new_book = Book.objects.create(
+            # user = request.user,
+            title = data["title"],
+            stock = int(data["stock"]),
+            author = data["author"],
+            publication_year  = int(data["publication_year"]),
+            publisher  = data["publisher"],
+            isbn  = data["isbn"],
+            image_s  = data["image_s"],
+            image_m  = data["image_m"],
+            image_l  = data["image_l"],
+            )
+               
+
+        new_book.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
+    
+
+@csrf_exempt
+def edit_book_flutter(request, id):
+    try:
+        data = Book.objects.get(pk=id)
+    except Book.DoesNotExist:
+        return JsonResponse({"status": "error", "message": "Buku tidak ditemukan"}, status=404)
+
+    if request.method == 'POST':
+        try:
+            updated_data = json.loads(request.body)
+            title = updated_data["title"]
+            author = updated_data["author"]
+            publication_year = updated_data["publication_year"]
+            publisher = updated_data["publisher"]
+            isbn = updated_data["isbn"]
+            image_s = updated_data["image_s"]
+            image_m = updated_data["image_m"]
+            image_l = updated_data["image_l"]
+            stock = updated_data["stock"]
+
+            if(title!=""):
+                data.title = title
+            if(author!=""):
+                data.author = author
+            if(publication_year!="" and publication_year!="0"):
+                data.publication_year = int(publication_year)
+            if(publisher!=""):
+                data.publisher = publisher
+            if(isbn!=""):
+                data.isbn = isbn
+            if(image_s!=""):
+                data.image_s = image_s
+            if(image_m!=""):
+                data.image_m = image_m
+            if(image_l!=""):
+                data.image_l = image_l
+            if(stock!="0" and stock !=""):
+                data.stock = int(stock)
+
+            data.save()
+
+            return JsonResponse({"status": "success"}, status=200)
+        except json.JSONDecodeError:
+            return JsonResponse({"status": "error", "message": "Format JSON tidak valid"}, status=400)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
+    
