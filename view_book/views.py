57a93701b2c8eeb9b75479fee5b835cf1669b6bb
almayawn <alma.laras@ui.ac.id>
2023-12-16 07:21:41 +0700

    updated views

--- view_book/views.py
+++ view_book/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, DetailView
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseNotFound, JsonResponse
@@ -50,18 +51,41 @@ class BookSearchView(ListView):
         context['q'] = self.request.GET.get('q')
         return context
 
+@csrf_exempt
 def add_to_cart_ajax(request, book_pk):
-    if request.method == 'POST':
+    if request.method == 'POST' or request.method == 'GET':
         user = request.user
         keranjang = get_object_or_404(Keranjang, user=user)
         book = Book.objects.filter(pk=book_pk).first()
         if book in keranjang.book_list.all():
-            return HttpResponseBadRequest("You have added book this to your cart")
+            return HttpResponse("Book is already in your cart", status=200)
         else:
             keranjang.book_list.add(book)
             keranjang.jumlah_buku = keranjang.book_list.count()
             keranjang.save()
             return HttpResponse(status=201)
+
+    return HttpResponseNotFound()
+
+@csrf_exempt
+def add_to_cart_flutter(request, book_pk):
+    if request.method == 'POST' or request.method == 'GET':
+        user = request.user
+        keranjang = get_object_or_404(Keranjang, user=user)
+        book = Book.objects.filter(pk=book_pk).first()
+        if book in keranjang.book_list.all():
+            return JsonResponse({
+                "status": False,
+                "message": "You have added this book to your cart."
+            }, status=200)
+        else:
+            keranjang.book_list.add(book)
+            keranjang.jumlah_buku = keranjang.book_list.count()
+            keranjang.save()
+            return JsonResponse({
+                "status": True,
+                "message": "Added to cart!"
+            }, status=200)
 
     return HttpResponseNotFound()
 
@@ -76,23 +100,59 @@ def get_review_json(request, book_pk):
 @csrf_exempt
 def give_review_ajax(request, book_pk):
     if request.method == 'POST':
-        book = Book.objects.get(pk=book_pk)
-        review = request.POST.get("review")
+        book = get_object_or_404(Book, pk=book_pk)
+        review_text = request.POST.get("review")
         rate = request.POST.get("rate")
         user = request.user
         try:
-            if int(rate) < 0 or int(rate) > 5:
+            rate = int(rate)
+            if rate < 0 or rate > 5:
                 return HttpResponseBadRequest("Invalid 'rate' value. Rate must be between 0 and 5.")
-        except:
-            return HttpResponseBadRequest("Invalid 'rate' value. Rate must be between 0 and 5.")
+        except ValueError:
+            return HttpResponseBadRequest("Invalid 'rate' value. Rate must be an integer between 0 and 5.")
+
+        existing_review = Review.objects.filter(book=book, user=user).first()
+
+        if existing_review:
+            existing_review.review = review_text
+            existing_review.rate = rate
+            existing_review.save()
+            
+            return HttpResponse("UPDATED", status=201)
         
+        else:
+            new_review = Review(book=book, user=user, review=review_text, rate=rate)
+            new_review.save()
 
-        new_review = Review(book=book, user=user, review=review, rate=rate)
-        new_review.save()
-
-        return HttpResponse(b"CREATED", status=201)
+            return HttpResponse("CREATED", status=201)
 
     return HttpResponseNotFound()
+
+@csrf_exempt
+def give_review_flutter(request, book_pk):
+    if request.method == 'POST':
+        book = get_object_or_404(Book, pk=book_pk)
+        data = json.loads(request.body)
+
+        review_text = data.get("review")
+        rate = int(data.get("rate"))
+        user = request.user
+        existing_review = Review.objects.filter(book=book, user=user).first()
+
+        if existing_review:
+            existing_review.review = review_text
+            existing_review.rate = rate
+            existing_review.save()
+            return JsonResponse({"status": "success", "existed": True}, status=200)
+        
+        else:
+            new_review = Review(book=book, user=user, review=review_text, rate=rate)
+            new_review.save()
+
+            return JsonResponse({"status": "success", "existed": False}, status=200)
+        
+    else:
+        return JsonResponse({"status": "error"}, status=401)
 
 @csrf_exempt
 def search_results(request):
