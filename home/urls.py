32550b52362003d9fc588902fe83555abba9ed6f
Photic23 <resellcourtesyx@gmail.com>
2023-12-16 11:30:56 +0700

    rev

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from home.views import register, logout_user, show_home, get_book_json, get_peminjaman_json, get_keranjang_json, submit_cart, show_keranjang, getBukuKeranjang_json, get_bukuPeminjaman_json, get_reviews_json, get_bukuReviews_json, remove_cart, show_profile
+from home.views import register, logout_user, show_home, get_book_json, get_peminjaman_json, get_keranjang_json, submit_cart, show_keranjang, getBukuKeranjang_json, get_bukuPeminjaman_json, get_reviews_json, get_bukuReviews_json, remove_cart, show_profile, getBukuKeranjangFlutter_json
 from django.conf import settings
 from django.conf.urls.static import static
 from django.conf import settings
@@ -19,7 +19,7 @@ urlpatterns = [
     path('remove-cart/', remove_cart, name='remove_cart'),
     path('cart/', show_keranjang, name='show_keranjang'),
     path('get-buku-keranjang/', getBukuKeranjang_json, name='getBukuKeranjang_json'),
-    path('get-buku-keranjang/<int:id>/', getBukuKeranjang_json, name='getBukuKeranjang_json'),
+    path('get-buku-keranjang/<int:id>/', getBukuKeranjang_json, name='getBukuKeranjangFlutter_json'),
     path('get-buku-peminjaman/', get_bukuPeminjaman_json, name='get_bukuPeminjaman_json'),
     path('get-reviews-peminjaman/', get_bukuReviews_json, name='get_bukuReviews_json'),
     path('show-profile/', show_profile, name='show_profile'),
